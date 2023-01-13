406f6b082b3cb3011844eb071cf488abad6449e8
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-01-13 22:12:25 +0100

    Adding sections and js

---
+++ Website/application.js
@@ -0,0 +1,36 @@
+const sections = document.querySelectorAll('.section');
+const sectButtons = document.querySelectorAll('.buttons');
+const sectButton = document.querySelectorAll('.button');
+const allSections = document.querySelector('.main');
+
+
+function PageTransition() { //Button Active
+    for(let i = 0; i < sectButton.length; i++) {
+        sectButton[i].addEventListener('click', function() {
+            let currentButton = document.querySelectorAll('.active-btn');
+            currentButton[0].className = currentButton[0].className.replace('active-btn', '');
+            this.className += ' active-btn'; //refering to this function
+        })
+    }
+
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){//removes selected from other buttons
+            sectButtons.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    }) 
+}
+
+
+PageTransition();
+
